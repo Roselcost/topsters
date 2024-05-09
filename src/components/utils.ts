bb16db18f621c0de840be642e42fc13395d55381
Roselcost <14353893+Roselcost@users.noreply.github.com>
2024-05-09 03:49:06 +0200

    Change dom-to-image to modern-screenshot

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,23 +1,12 @@
-import domtoimage from "dom-to-image";
+import { domToPng } from "modern-screenshot";
 
 export const downloadImage = async (title: string) => {
-  const elem = document.getElementById("imageContainer")!;
-  const scale = 4;
-  await domtoimage
-    .toPng(elem, {
-      width: elem.scrollWidth * scale,
-      height: elem.scrollHeight * scale,
-      style: { transform: `scale(${scale})`, transformOrigin: "top left" },
-    })
-    .then(function (dataUrl) {
-      const img = new Image();
-      img.src = dataUrl;
+  await domToPng(document.getElementById("imageContainer")!, { scale: 4 }).then(
+    (dataUrl) => {
       const link = document.createElement("a");
       link.download = `${!!title ? title : "untitled"}.png`;
       link.href = dataUrl;
       link.click();
-    })
-    .catch(function (error) {
-      console.error("oops, something went wrong!", error);
-    });
+    }
+  );
 };
