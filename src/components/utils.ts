9d1ff685c52b3e6d3b8434240a5dcfa8e3586802
Roselcost <14353893+Roselcost@users.noreply.github.com>
2024-05-14 15:58:42 +0200

    Reduce generated image size

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,7 +1,7 @@
 import { domToPng } from "modern-screenshot";
 
 export const downloadImage = async (title: string) => {
-  await domToPng(document.getElementById("imageContainer")!, { scale: 4 }).then(
+  await domToPng(document.getElementById("imageContainer")!, { scale: 2 }).then(
     (dataUrl) => {
       const link = document.createElement("a");
       link.download = `${!!title ? title : "untitled"}.png`;
